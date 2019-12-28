de562fba7e33942715ecf0cc5c0365f99b7a6697
Volodymyr Tsurko <chegnenado@yahoo.com>
2019-12-28 11:42:01 +0200

    Initial commit.

---
+++ Loscialo_Intro to TDDTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loscialo_Intro_to_TDDTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
